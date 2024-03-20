d4a7fedca9d9641edc47d43c10dea274e0fc6cb3
antares <ahmet_emin_serttas@hotmail.com>
2024-03-20 21:09:56 +0300

    add authController

---
+++ BorkarEmlakUI/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using BorkarEmlak.DATA.Concrate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BorkarEmlakUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager; 
+        private readonly SignInManager<AppUser> _signInManager;     
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();  
+        }
+    }
+}
--- BorkarEmlakUI/Program.cs
+++ BorkarEmlakUI/Program.cs
@@ -1,3 +1,6 @@
+using BorkarEmlak.DATA.Concrate;
+using BorkarEmlak.REPO.Context;
+
 namespace BorkarEmlakUI
 {
     public class Program
@@ -6,6 +9,15 @@ namespace BorkarEmlakUI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<ApplicationContext>();
+            builder.Services.AddIdentity<AppUser, AppRole>(
+                ).AddEntityFrameworkStores<ApplicationContext>(); //contexteki ozellikleri de dahil etmesi için eklenmesi gerekiyor.
+            builder.Services.ConfigureApplicationCookie(
+                option =>
+                {
+                    option.LoginPath = "/Auth/Login";
+                }
+            );
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
